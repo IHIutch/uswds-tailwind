e694c178da975cbb6c82814b14367814be95e273
JB Hutch <jbhutch01@gmail.com>
2023-12-03 20:17:42 -0500

    Update storybook preview sizes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,6 +12,52 @@ const preview = {
         date: /Date$/,
       },
     },
+    viewport: {
+      viewports: {
+        mobile: {
+          name: 'Mobile',
+          styles: {
+            width: '320px',
+            height: '568px',
+          },
+        },
+        mobileLg: {
+          name: 'Mobile Large',
+          styles: {
+            width: '480px',
+            height: '960px',
+          },
+        },
+        tablet: {
+          name: 'Tablet',
+          styles: {
+            width: '640px',
+            height: '360px',
+          },
+        },
+        tabletLg: {
+          name: 'Tablet Large',
+          styles: {
+            width: '880px',
+            height: '495px',
+          },
+        },
+        desktop: {
+          name: 'Desktop',
+          styles: {
+            width: '1024px',
+            height: '768px',
+          },
+        },
+        desktopLg: {
+          name: 'Desktop Large',
+          styles: {
+            width: '1200px',
+            height: '900px',
+          },
+        },
+      }
+    }
   },
 };
 
