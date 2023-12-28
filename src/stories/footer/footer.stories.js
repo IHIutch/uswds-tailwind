420d7fba31f49f5e2d6231d3b655616c1ca4739b
JB Hutch <jbhutch01@gmail.com>
2023-12-27 19:44:26 -0500

    Initial footers

--- src/stories/footer/footer.stories.js
+++ src/stories/footer/footer.stories.js
@@ -1,9 +1,19 @@
+import FooterDefault from '../../components/footer/examples/footer.twig'
+import FooterMedium from '../../components/footer/examples/medium.twig'
+import FooterSlim from '../../components/footer/examples/slim.twig'
+// import props from "./footer-props.json";
 
 export default {
-  title: '_Footer',
+  title: 'Footer',
 };
 
 export const Default = {
-  render: () => '<div>todo</div>'
+  render: () => FooterDefault()
+};
+export const Medium = {
+  render: () => FooterMedium()
+};
+export const Slim = {
+  render: () => FooterSlim()
 };
 
