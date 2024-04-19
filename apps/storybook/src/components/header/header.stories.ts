43cd1375cc403e33cd427c3e492d1219dec9d012
JB Hutch <jbhutch01@gmail.com>
2024-04-19 11:41:44 -0400

    Add extended header and update header schema

--- apps/storybook/src/components/header/header.stories.ts
+++ apps/storybook/src/components/header/header.stories.ts
@@ -1,5 +1,7 @@
 import { renderTwig } from "../../utils/render-twig";
 import HeaderDefault from "./examples/header.twig?url";
+import HeaderExtended from "./examples/header-extended.twig?url";
+
 import props from "./header-props.json";
 
 export default {
@@ -7,5 +9,9 @@ export default {
 };
 
 export const Default = {
-  render: () => renderTwig({path: HeaderDefault, props}),
+  render: () => renderTwig({ path: HeaderDefault, props }),
+};
+
+export const Extended = {
+  render: () => renderTwig({ path: HeaderExtended, props }),
 };
