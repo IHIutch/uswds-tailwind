09f3fd3abaed1b59543bba741c322249cd8fe0d1
Jonathan Hutchison <jbhutch01@gmail.com>
2024-06-28 13:43:32 -0400

    Update getting started, code block styling

---
+++ apps/docs/astro.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
+// @ts-expect-error
+import tailwindNesting from '@tailwindcss/nesting';
+import expressiveCode from "astro-expressive-code";
+
+// https://astro.build/config
+export default defineConfig({
+  trailingSlash: 'never',
+  integrations: [
+    expressiveCode({
+      themes: ['light-plus', 'dark-plus'],
+      styleOverrides: {
+        frames: {
+          editorBackground: '#f7f9fa',
+          terminalBackground: '#f7f9fa',
+          terminalTitlebarBackground: '#ffffff',
+          frameBoxShadowCssValue: '0'
+        },
+        codeBackground: '#f7f9fa'
+      }
+    }),
+    tailwind(),
+    mdx(),
+    alpinejs({
+      entrypoint: "/src/utils/alpine/entrypoint"
+    })
+  ],
+  // markdown: {
+  //   shikiConfig: {
+  //     themes: {
+  //       light: 'light-plus',
+  //       dark: 'dark-plus'
+  //     }
+  //   }
+  // },
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwindNesting()]
+      }
+    }
+  },
+  redirects: {
+    '/components': '/components/accordion'
+  }
+});
--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -3,12 +3,12 @@ import sharedConfig from "@repo/tailwind-config";
 import tailwindTypography from '@tailwindcss/typography'
 
 const config: Config = {
-  content: ['./src/**/*.{astro,html,ts,tsx,twig}'],
+  content: ['./src/**/*.{astro,html,ts,tsx,twig,mdx,md}'],
   presets: [sharedConfig],
   plugins: [
     tailwindTypography({
       className: 'docs-prose'
-    })
+    }),
   ],
 };
 
