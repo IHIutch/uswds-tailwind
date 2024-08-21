16853f9f55558effa56aa7ec763681b32713404b
Jonathan Hutchison <jbhutch01@gmail.com>
2024-08-21 12:45:58 -0400

    Use debounce to make search nicer. Add a not found state

--- apps/docs/src/components/icon-list.tsx
+++ apps/docs/src/components/icon-list.tsx
@@ -2,9 +2,11 @@ import * as React from 'react'
 import { icons as materialIcons } from "@iconify-json/material-symbols";
 import Fuse from "fuse.js";
 import { getIcons } from '@iconify/utils';
+import { useDebounce } from "@uidotdev/usehooks";
 
-const icons = Object.keys(materialIcons.icons);
-const fuse = new Fuse(icons, {
+const SEARCH_LIMIT = 250
+const iconList = Object.keys(materialIcons.icons)
+const fuse = new Fuse(iconList, {
   // isCaseSensitive: false,
   // includeScore: true,
   // shouldSort: true,
@@ -24,22 +26,41 @@ const fuse = new Fuse(icons, {
   // ]
 });
 
-const SEARCH_LIMIT = 250
-
 export default function IconList() {
   const [search, setSearch] = React.useState('')
-  const searchResults = fuse.search(search, {
-    limit: SEARCH_LIMIT
-  })
-  // const searchResults = icons.filter(i => i.startsWith(search))
-  const filteredIconsJson = getIcons(materialIcons, search
-    ? searchResults.map(i => i.item)
-    : icons.slice(0, SEARCH_LIMIT)
-  );
-  let filteredIconsList = Object.keys(filteredIconsJson?.icons || {})
-  if (!search) {
-    filteredIconsList.sort()
+  const [filteredIcons, setFilteredIcons] = React.useState<string[]>([])
+  const debouncedSearch = useDebounce(search, 300);
+
+  const copyTextToClipboard = async (text: string) => {
+    if ('clipboard' in navigator) {
+      return await navigator.clipboard.writeText(text);
+    } else {
+      return document.execCommand('copy', true, text);
+    }
   }
+
+  React.useEffect(() => {
+    const handleSearch = async () => {
+      // setIsSearching(true);
+
+      const searchResults = fuse.search(debouncedSearch, {
+        limit: SEARCH_LIMIT
+      })
+
+      const filteredIconsJson = getIcons(materialIcons, debouncedSearch
+        ? searchResults.map(i => i.item)
+        : iconList.slice(0, SEARCH_LIMIT)
+      );
+
+      const filteredIconsList = Object.keys(filteredIconsJson?.icons || {})
+      if (!debouncedSearch) {
+        filteredIconsList.sort()
+      }
+      // setIsSearching(false);
+      setFilteredIcons(filteredIconsList);
+    };
+    handleSearch()
+  }, [debouncedSearch]);
 
   return (
     <div className="border border-gray-cool-20">
@@ -55,28 +76,43 @@ export default function IconList() {
             />
           </div>
         </div>
-        <p aria-live="polite" className="text-gray-50">{filteredIconsList.length} of {icons.length} icons. Click an icon to copy its codesnippet.</p>
+        <p aria-live="polite" className="text-gray-50">{filteredIcons.length} of {iconList.length} icons. Click an icon to copy its class name.</p>
       </div>
-      <div className="grid grid-cols-5 gap-3 bg-gray-cool-2 p-4">
+      <div className="bg-gray-cool-2 p-4">
         {
-          filteredIconsList.map((mi) => (
-            <button key={mi} className="text-gray-cool-80 gap-1 aspect-square border border-gray-cool-10 bg-white rounded flex flex-col items-center justify-center p-2 shadow hover:bg-gray-cool-2 focus:outline focus:outline-4 focus:outline-blue-40v">
-              <div className="size-8">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="100%"
-                  height="100%"
-                  viewBox="0 0 24 24"
-                  dangerouslySetInnerHTML={{
-                    __html: materialIcons.icons[mi].body
-                  }}
-                />
-              </div>
-              <div className="text-ellipsis whitespace-nowrap overflow-hidden w-full">
-                <span className="text-xs">{mi}</span>
-              </div>
-            </button>
-          ))
+          filteredIcons.length > 0 ? (
+            <div className="grid grid-cols-3 gap-3">
+              {filteredIcons.map((mi) => (
+                <button
+                  key={mi}
+                  className="text-gray-cool-80 gap-2 border border-gray-cool-10 bg-white rounded flex flex-col items-center justify-center p-4 shadow hover:bg-gray-cool-2 focus:outline focus:outline-4 focus:outline-blue-40v"
+                  onClick={() => copyTextToClipboard('icon-[material-symbols--' + mi + ']')}
+                  title={'.icon-[material-symbols--' + mi + ']'}
+                >
+                  <div className="size-8">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="100%"
+                      height="100%"
+                      viewBox="0 0 24 24"
+                      dangerouslySetInnerHTML={{
+                        __html: materialIcons.icons[mi].body
+                      }}
+                    />
+                  </div>
+                  <div className="truncate text-ellipsis w-full">
+                    <span aria-hidden className="text-xs">{'.icon-[material-symbols--' + mi + ']'}</span>
+                    <span className="sr-only">{mi.replaceAll('-', ' ')}</span>
+                  </div>
+                </button>
+
+              ))}
+            </div>
+          ) : (
+            <div className="text-center w-full py-4">
+              <span className="text-gray-cool-70">No icons matched your search</span>
+            </div>
+          )
         }
       </div>
     </div>
