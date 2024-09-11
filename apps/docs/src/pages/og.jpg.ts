7f510504275a8d1ec0b4b68779e78f5a7b3ecbfd
Jonathan Hutchison <jbhutch01@gmail.com>
2024-09-11 10:19:15 -0400

    Fix meta image

--- apps/docs/src/pages/og.jpg.ts
+++ apps/docs/src/pages/og.jpg.ts
@@ -11,7 +11,7 @@ export const GET: APIRoute = async ({ url }) => {
     await generateOgImage({
       title: paramTitle || 'USWDS + Tailwind',
       description: paramDescription || 'Build federal websites and applications faster than ever using the latest in modern tooling.',
-      isHome: !paramTitle
+      isHome: paramTitle === 'USWDS + Tailwind'
     }),
     {
       headers: { 'Content-Type': 'image/jpg' },
