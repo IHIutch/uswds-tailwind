b2b058300ca393686d1dfffd85956d67787deca1
Jonathan Hutchison <jbhutch01@gmail.com>
2024-09-16 09:25:25 -0400

    Update storybook

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,22 +1,25 @@
+import { dirname, join } from "path";
 const config = {
   stories: ["../src/components/**/*.stories.@(js|ts)"],
 
   staticDirs: ['../public'],
 
   addons: [
-    "@storybook/addon-a11y",
-    "@storybook/addon-essentials",
-    "@storybook/addon-links",
-    "@chromatic-com/storybook",
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@chromatic-com/storybook"),
   ],
 
   framework: {
-    name: "@storybook/html-vite",
+    name: getAbsolutePath("@storybook/html-vite"),
     options: {},
   },
 
-  docs: {
-    autodocs: false
-  }
+  docs: {}
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
