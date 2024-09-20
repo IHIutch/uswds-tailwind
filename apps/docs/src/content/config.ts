c9bb20c2c337f94327acc8b124a527f24002d78c
Jonathan Hutchison <jbhutch01@gmail.com>
2024-09-20 17:27:33 -0400

    Unpublish validation component

--- apps/docs/src/content/config.ts
+++ apps/docs/src/content/config.ts
@@ -4,7 +4,8 @@ const componentsCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    description: z.string()
+    description: z.string(),
+    isPublished: z.boolean().optional()
   }),
 })
 
