3da1949a5fa252cfde4d12fb6c3fed6eeaf0c451
JB Hutch <jbhutch01@gmail.com>
2025-11-02 22:39:58 -0500

    Handle collapsible code default size when snippet isnt tall enough to collapse

--- apps/docs/src/components/collapsible-code.tsx
+++ apps/docs/src/components/collapsible-code.tsx
@@ -1,27 +1,45 @@
-import { Collapsible } from '@ark-ui/react/collapsible'
+import { Collapsible, useCollapsible } from '@ark-ui/react/collapsible'
+import * as React from 'react'
 
 export default function CollapsibleCode({ className, children }: {
   className?: string
   children?: React.ReactNode
 }) {
+  const openSize = 384
+  const wrapperRef = React.useRef<HTMLDivElement>(null)
+  const collapsible = useCollapsible({
+    collapsedHeight: `${openSize}px`,
+    defaultOpen: true,
+  })
+
+  React.useEffect(() => {
+    if ((wrapperRef.current?.offsetHeight || 0) > openSize) {
+      collapsible.setOpen(false)
+    }
+  }, [collapsible])
+
   return (
-    <Collapsible.Root collapsedHeight="24rem" className={`relative overflow-hidden ${className}`}>
-      <Collapsible.Content>
+    <Collapsible.RootProvider value={collapsible} className={`relative overflow-hidden ${className}`}>
+      <Collapsible.Content style={{ maxHeight: `${openSize}px`, overflow: 'auto' }}>
         <div
           className="overflow-hidden not-docs-prose"
+          ref={wrapperRef}
         >
           {children}
         </div>
       </Collapsible.Content>
-      <Collapsible.Trigger
-        className="group flex justify-center items-end h-32 pb-3 absolute inset-x-0 bottom-0 bg-gradient-to-t from-gray-cool-2/100 from-[24px] to-gray-cool-2/0 border-gray-cool-20 border-b border-x rounded-b outline-none data-[state=open]:hidden"
+      <button
+        type="button"
+        onClick={() => collapsible.setOpen(true)}
+        className="group flex justify-center items-end h-32 pb-3 absolute inset-x-0 bottom-0 bg-gradient-to-t from-gray-cool-2/100 from-[24px] to-gray-cool-2/0 border-gray-cool-20 border-b border-x rounded-b outline-none"
+        hidden={collapsible.open}
       >
         <div
-          className="flex items-center gap-2 rounded font-bold leading-none text-white px-5 py-3 bg-blue-60v group-hover:bg-blue-warm-70v group-active:bg-blue-warm-80v group-focus:outline-4 group-focus:outline-offset-4 group-focus:outline-blue-40v"
+          className="flex items-center gap-2 rounded font-bold leading-none cursor-pointer text-white px-5 py-3 bg-blue-60v group-hover:bg-blue-warm-70v group-active:bg-blue-warm-80v group-focus:outline-4 group-focus:outline-offset-4 group-focus:outline-blue-40v"
         >
           <span>Expand Code</span>
         </div>
-      </Collapsible.Trigger>
-    </Collapsible.Root>
+      </button>
+    </Collapsible.RootProvider>
   )
 }
