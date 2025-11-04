2edcd1bfe27d3fb240b925261dd67c11809cca65
JB Hutch <jbhutch01@gmail.com>
2025-11-04 01:42:13 +0000

    Use zag directly

--- apps/docs/src/components/collapsible-code.tsx
+++ apps/docs/src/components/collapsible-code.tsx
@@ -1,45 +1,50 @@
-import { Collapsible, useCollapsible } from '@ark-ui/react/collapsible'
+import { normalizeProps, useMachine } from '@zag-js/react'
+import * as toggle from '@zag-js/toggle'
 import * as React from 'react'
 
 export default function CollapsibleCode({ className, children }: {
   className?: string
   children?: React.ReactNode
 }) {
-  const openSize = 384
+  const [isMounted, setIsMounted] = React.useState(false)
   const wrapperRef = React.useRef<HTMLDivElement>(null)
-  const collapsible = useCollapsible({
-    collapsedHeight: `${openSize}px`,
-    defaultOpen: true,
-  })
+  const service = useMachine(toggle.machine, { defaultPressed: false })
+  const api = toggle.connect(service, normalizeProps)
 
   React.useEffect(() => {
-    if ((wrapperRef.current?.offsetHeight || 0) > openSize) {
-      collapsible.setOpen(false)
+    if (wrapperRef.current) {
+      setIsMounted(true)
+      if (wrapperRef.current.offsetHeight < 384) {
+        api.setPressed(true)
+      }
     }
-  }, [collapsible.isUnmounted])
+  }, [wrapperRef.current])
 
   return (
-    <Collapsible.RootProvider value={collapsible} className={`relative overflow-hidden ${className}`}>
-      <Collapsible.Content style={!wrapperRef.current ? { maxHeight: `${openSize}px`, overflow: 'auto' } : undefined}>
+    <div className={`relative overflow-hidden ${className}`}>
+      <div className={!wrapperRef.current ? 'max-h-96 overflow-auto' : api.pressed ? 'h-auto' : 'h-96'}>
         <div
-          className="overflow-hidden not-docs-prose"
+          className="not-docs-prose"
           ref={wrapperRef}
         >
           {children}
         </div>
-      </Collapsible.Content>
-      <button
-        type="button"
-        onClick={() => collapsible.setOpen(true)}
-        className="group flex justify-center items-end h-32 pb-3 absolute inset-x-0 bottom-0 bg-gradient-to-t from-gray-cool-2/100 from-[24px] to-gray-cool-2/0 border-gray-cool-20 border-b border-x rounded-b outline-hidden"
-        hidden={collapsible.open}
-      >
-        <div
-          className="flex items-center gap-2 rounded-sm font-bold leading-none cursor-pointer text-white px-5 py-3 bg-blue-60v group-hover:bg-blue-warm-70v group-active:bg-blue-warm-80v group-focus:outline-4 group-focus:outline-offset-4 group-focus:outline-blue-40v"
-        >
-          <span>Expand Code</span>
-        </div>
-      </button>
-    </Collapsible.RootProvider>
+      </div>
+      {isMounted
+        ? (
+            <button
+              type="button"
+              className="group flex justify-center items-end h-32 pb-3 absolute inset-x-0 bottom-0 bg-gradient-to-t from-gray-cool-2/100 from-[24px] to-gray-cool-2/0 border-gray-cool-20 border-b border-x rounded-b outline-hidden data-[pressed]:hidden"
+              {...api.getRootProps()}
+            >
+              <div
+                className="flex items-center gap-2 rounded-sm font-bold leading-none cursor-pointer text-white px-5 py-3 bg-blue-60v group-hover:bg-blue-warm-70v group-active:bg-blue-warm-80v group-focus:outline-4 group-focus:outline-offset-4 group-focus:outline-blue-40v"
+              >
+                <span>Expand Code</span>
+              </div>
+            </button>
+          )
+        : null}
+    </div>
   )
 }
--- apps/docs/src/components/zag-splitter.tsx
+++ apps/docs/src/components/zag-splitter.tsx
@@ -1,32 +1,33 @@
-import { Splitter } from '@ark-ui/react/splitter'
+import { normalizeProps, useMachine } from '@zag-js/react'
+import * as splitter from '@zag-js/splitter'
+import * as React from 'react'
 
 export default function ZagSplitter({ resize = true, children }: { resize: boolean, children: React.ReactNode }) {
-  return (
-    <Splitter.Root
-      defaultSize={[100, 0]}
-      panels={[{ id: 'a', collapsible: resize }, { id: 'b' }]}
-    >
-      <Splitter.Panel
-        id="a"
-        className="bg-gray-cool-2 shadow-[1px_0px_0px_#dfe1e2] p-4"
-      >
-        {children}
-      </Splitter.Panel>
-      {
-        resize
-          ? (
-            <Splitter.ResizeTrigger
-              id="a:b"
-              aria-label="Resize"
-              className="items-center p-2 group outline-hidden hidden tablet:flex"
-            >
-              <div className="flex max-h-32 h-full rounded-full w-1.5 bg-blue-60v group-hover:bg-blue-warm-70v group-active:bg-blue-warm-80v group-active:outline-4 group-active:outline-offset-4 group-active:outline-blue-40v group-focus:outline-4 group-focus:outline-offset-4 group-focus:outline-blue-40v" />
-            </Splitter.ResizeTrigger>
-          )
-          : null
-      }
+  const service = useMachine(splitter.machine, {
+    id: React.useId(),
+    defaultSize: [100, 0],
+    panels: [
+      { id: 'a', collapsible: resize },
+      { id: 'b' },
+    ],
+  })
 
-      {resize ? <Splitter.Panel id="b" /> : null}
-    </Splitter.Root>
+  const api = splitter.connect(service, normalizeProps)
+
+  return (
+    <div {...api.getRootProps()}>
+      <div {...api.getPanelProps({ id: 'a' })} className="bg-gray-cool-2 shadow-[1px_0px_0px_#dfe1e2] p-4">
+        {children}
+      </div>
+
+      {resize
+        ? (
+            <div {...api.getResizeTriggerProps({ id: 'a:b' })} className="items-center p-2 group outline-hidden hidden tablet:flex">
+              <div className="flex max-h-32 h-full rounded-full w-1.5 bg-blue-60v group-hover:bg-blue-warm-70v group-active:bg-blue-warm-80v group-active:outline-4 group-active:outline-offset-4 group-active:outline-blue-40v group-focus:outline-4 group-focus:outline-offset-4 group-focus:outline-blue-40v" />
+            </div>
+          )
+        : null}
+      <div {...api.getPanelProps({ id: 'b' })}></div>
+    </div>
   )
 }
