cec5c1a118628b388db22364700cc7b6b467f0e4
JB Hutch <jbhutch01@gmail.com>
2025-11-09 18:41:18 -0500

    Allow overflow of non resizable component previews

--- apps/docs/src/components/zag-splitter.tsx
+++ apps/docs/src/components/zag-splitter.tsx
@@ -3,6 +3,16 @@ import * as splitter from '@zag-js/splitter'
 import * as React from 'react'
 
 export default function ZagSplitter({ resize = true, children }: { resize: boolean, children: React.ReactNode }) {
+  if (!resize) {
+    return (
+      <div
+        className="bg-gray-cool-2 shadow-[1px_0px_0px_#dfe1e2] p-4"
+      >
+        {children}
+      </div>
+    )
+  }
+
   const service = useMachine(splitter.machine, {
     id: React.useId(),
     defaultSize: [100, 0],
@@ -15,18 +25,20 @@ export default function ZagSplitter({ resize = true, children }: { resize: boole
   const api = splitter.connect(service, normalizeProps)
 
   return (
-    <div {...api.getRootProps()}>
-      <div {...api.getPanelProps({ id: 'a' })} className="bg-gray-cool-2 shadow-[1px_0px_0px_#dfe1e2] p-4">
+    <div
+      {...api.getRootProps()}
+    >
+      <div
+        {...api.getPanelProps({ id: 'a' })}
+        className="bg-gray-cool-2 shadow-[1px_0px_0px_#dfe1e2] p-4"
+      >
         {children}
       </div>
 
-      {resize
-        ? (
-            <div {...api.getResizeTriggerProps({ id: 'a:b' })} className="items-center p-2 group outline-hidden hidden tablet:flex">
-              <div className="flex max-h-32 h-full rounded-full w-1.5 bg-blue-60v group-hover:bg-blue-warm-70v group-active:bg-blue-warm-80v group-active:outline-4 group-active:outline-offset-4 group-active:outline-blue-40v group-focus:outline-4 group-focus:outline-offset-4 group-focus:outline-blue-40v" />
-            </div>
-          )
-        : null}
+      <div {...api.getResizeTriggerProps({ id: 'a:b' })} className="items-center p-2 group outline-hidden hidden tablet:flex">
+        <div className="flex max-h-32 h-full rounded-full w-1.5 bg-blue-60v group-hover:bg-blue-warm-70v group-active:bg-blue-warm-80v group-active:outline-4 group-active:outline-offset-4 group-active:outline-blue-40v group-focus:outline-4 group-focus:outline-offset-4 group-focus:outline-blue-40v" />
+      </div>
+
       <div {...api.getPanelProps({ id: 'b' })}></div>
     </div>
   )
