b10e18bfac9b8db65e785f1401bc5af2eff067e6
JB Hutch <jbhutch01@gmail.com>
2026-03-22 15:59:34 -0400

    Summary box

---
+++ packages/react/src/summary-box/summary-box.stories.tsx
@@ -0,0 +1,23 @@
+import preview from '../../.storybook/preview'
+import { SummaryBox } from './summary-box'
+
+const meta = preview.meta({
+  tags: ['new'],
+  title: 'Components/Summary Box',
+  component: SummaryBox.Root,
+})
+
+export const Default = meta.story({
+  render: () => (
+    <SummaryBox.Root>
+      <SummaryBox.Heading>Key information</SummaryBox.Heading>
+      <SummaryBox.Content className="prose">
+        <ul>
+          <li>If you are under 18, you cannot apply.</li>
+          <li>You must be a U.S. citizen or permanent resident.</li>
+          <li>You must have a valid Social Security number.</li>
+        </ul>
+      </SummaryBox.Content>
+    </SummaryBox.Root>
+  ),
+})
---
+++ packages/react/src/summary-box/summary-box.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+import { cx } from '../cva.config'
+
+interface SummaryBoxContextProps {
+  headingId: string
+}
+
+const SummaryBoxContext = React.createContext<SummaryBoxContextProps | null>(null)
+
+function useSummaryBoxContext() {
+  const context = React.useContext(SummaryBoxContext)
+  if (!context) {
+    throw new Error('SummaryBox components must be used within a SummaryBox.Root')
+  }
+  return context
+}
+
+type SummaryBoxRootProps = React.HTMLAttributes<HTMLDivElement>
+
+function SummaryBoxRoot({ className, ...props }: SummaryBoxRootProps) {
+  const headingId = React.useId()
+  return (
+    <SummaryBoxContext.Provider value={{ headingId }}>
+      <div
+        role="complementary"
+        aria-labelledby={headingId}
+        {...props}
+        className={cx('border border-cyan-20 bg-cyan-5 rounded-sm p-6', className)}
+      />
+    </SummaryBoxContext.Provider>
+  )
+}
+
+type SummaryBoxHeadingProps = React.HTMLAttributes<HTMLHeadingElement>
+
+function SummaryBoxHeading({ className, ...props }: SummaryBoxHeadingProps) {
+  const { headingId } = useSummaryBoxContext()
+  return (
+    <div
+      id={headingId}
+      {...props}
+      className={cx('text-xl font-bold', className)}
+    />
+  )
+}
+
+type SummaryBoxContentProps = React.HTMLAttributes<HTMLDivElement>
+
+function SummaryBoxContent({ className, ...props }: SummaryBoxContentProps) {
+  return (
+    <div
+      {...props}
+      className={cx('mt-4', className)}
+    />
+  )
+}
+
+export const SummaryBox = {
+  Root: SummaryBoxRoot,
+  Heading: SummaryBoxHeading,
+  Content: SummaryBoxContent,
+}
