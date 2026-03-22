d171e4d3659de56d079aec9d6112972332b2a743
JB Hutch <jbhutch01@gmail.com>
2026-03-22 16:15:57 -0400

    Tag

---
+++ packages/react/src/tag/tag.tsx
@@ -0,0 +1,27 @@
+import type { VariantProps } from 'cva'
+import * as React from 'react'
+import { cva, cx } from '../cva.config'
+
+export const tagVariants = cva({
+  base: 'bg-gray-60 text-white rounded-xs py-px px-2 uppercase',
+  variants: {
+    size: {
+      md: 'text-sm',
+      lg: 'text-base',
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+  },
+})
+
+export type TagProps = React.HTMLAttributes<HTMLSpanElement> & VariantProps<typeof tagVariants>
+
+export function Tag({ className, size, ...props }: TagProps) {
+  return (
+    <span
+      {...props}
+      className={cx(tagVariants({ size, className }))}
+    />
+  )
+}
